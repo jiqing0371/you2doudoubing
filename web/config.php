3ef6308596dc9f79bfb9fe5350ff3cdaecf88fa6
jiqing0371 <42863237+jiqing0371@users.noreply.github.com>
2018-08-31 18:16:31 +0800

    Update config.php
    web/config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
 define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
+define('SITE_NAME', 'youguanshipin');
+define('TITLENAME', 'herokuyoutubephp');
 define('EN2DEKEY', 'lujianbupingyishenghou');
 define('EMAIL', 'xiaofufua@protonmail.com');
 ?>
